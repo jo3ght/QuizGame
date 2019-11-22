ec40d1d51506c592135ce96f705963d7e9ef6efa
jo3ght <needgloomy@gmail.com>
2019-11-22 21:29:28 +0700

    Fix the image bug 2

--- app/src/main/java/com/example/quizgame/Mode.java
+++ app/src/main/java/com/example/quizgame/Mode.java
@@ -14,7 +14,7 @@ import com.example.quizgame.Database.Question;
 public class Mode extends AppCompatActivity {
 
     public Spinner spinnerLevel;
-
+    //ok
     public static final String EXTRA_LEVEL = "extraLevel";
 
     @Override
